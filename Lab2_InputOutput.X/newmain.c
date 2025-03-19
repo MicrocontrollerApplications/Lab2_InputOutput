607dce40e80784b88cccf195c7780cbdd3821885
Sebastian Moosbauer <sebastian.moosbauer@thu.de>
2025-03-19 11:45:58 +0100

    add code snippet for lab session

--- Lab2_InputOutput.X/newmain.c
+++ Lab2_InputOutput.X/newmain.c
@@ -9,5 +9,29 @@
 #include <xc.h>
 
 void main(void) {
-    return;
+    // initialize microcontroller
+    __init();
+    
+    unsigned char button;
+    
+    while(1){
+        button = PORTBbits.RB2; // Read button TL
+        
+        if (button == 0) { // TL pressed?
+            LATBbits.LATB3 = 0; // LED2 on
+        } else{
+            LATBbits.LATB3 = 1;  // LED2 off
+        }
+        
+        if (PORTBbits.RB4 == 0) { // TR pressed?
+            while (PORTBbits.RB4 == 0) { // wait until TR is not pressed anymore
+                Nop();
+            }
+            
+            LATBbits.LATB5 = !LATBbits.LATB5; //toggle LED4
+        }
+        Nop(); // Do nothing, a breakpoint may be placed here.
+    }
+    
+    return 0;
 }
