4417aed2fb31a1266bdc7a00783f7adf3c93783a
Sebastian Moosbauer <sebastian.moosbauer@thu.de>
2025-03-21 11:57:57 +0100

    fix issues

--- Lab2_InputOutput.X/main.c
+++ Lab2_InputOutput.X/main.c
@@ -13,7 +13,7 @@
  */
 void __init(void);
 
-void main(void) {
+int main(void) {
     // initialize microcontroller
     __init();
     
@@ -44,6 +44,6 @@ void main(void) {
 void __init(void){
     OSCCON = 0x50; // 500kHz internal clock (we'll learn that soon!))
     ANSELB = 0; // set all pins in register B to be digital
-    TRISB = 0b00001010; //Input: RB2 and RB4; Output: all others
-    LATB = 0b00001010; // Turn LEDs off
+    TRISB = 0b00010100; //Input: RB2 and RB4; Output: all others
+    LATB = 0b00010100; // Turn LEDs off
 }
