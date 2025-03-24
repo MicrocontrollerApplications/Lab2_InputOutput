6578553ed63afaef81c3c33c0e528b102381149c
Sebastian Moosbauer <sebastian.moosbauer@thu.de>
2025-03-24 08:44:46 +0100

    Feature initial version (#1)
    * add code snippet for lab session
    
    * add debug table to README
    
    * renamed newmain.c to main.c
    
    * enhance README.md's format
    
    * add gif to README
    
    * add textual descrition to open debug watch
    
    * finish draft of Lab2
    
    * fix formating issue
    
    * Update README.md
    
    * fix issues
    
    * remove unused libraries
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * fix Sebastian's typo
    
    * add hint for TRISB register
    
    ---------
    
    Co-authored-by: Badura-m <merle.badura@thu.de>

---
+++ Lab2_InputOutput.X/main.c
@@ -0,0 +1,49 @@
+/*
+ * File:   newmain.c
+ * Author: Sebastian Moosbauer
+ *
+ * Created on March 19, 2025, 11:24 AM
+ */
+
+
+#include <xc.h>
+
+/**
+ * Initialization function for our program. You'll learn to extend this today :)
+ */
+void __init(void);
+
+int main(void) {
+    // initialize microcontroller
+    __init();
+    
+    unsigned char button;
+    
+    while(1){
+        button = PORTBbits.RB2; // Read button TL
+        
+        if (button == 0) { // TL pressed?
+            LATBbits.LATB3 = 0; // LED2 on
+        } else{
+            LATBbits.LATB3 = 1;  // LED2 off
+        }
+        
+        if (PORTBbits.RB4 == 0) { // TR pressed?
+            while (PORTBbits.RB4 == 0) { // wait until TR is not pressed anymore
+                Nop();
+            }
+            
+            LATBbits.LATB5 = !LATBbits.LATB5; //toggle LED4
+        }
+        Nop(); // Do nothing, a breakpoint may be placed here.
+    }
+    
+    return 0;
+}
+
+void __init(void){
+    OSCCON = 0x50; // 500kHz internal clock (we'll learn that soon!))
+    ANSELB = 0; // set all pins in register B to be digital
+    TRISB = 0b00010100; //Input: RB2 and RB4; Output: all others
+    LATB = 0b00010100; // Turn LEDs off
+}
